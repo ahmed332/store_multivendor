2d0a8ff89b08fe789117e2791ebb4e45e6fcf45d
ahmed332 <ahmedmonsef787@gmail.com>
2025-08-06 09:44:56 +0300

    config product page

--- resources/views/Front/product/show.blade.php
+++ resources/views/Front/product/show.blade.php
@@ -11,7 +11,7 @@
                 <div class="col-lg-6 col-md-6 col-12">
                     <ul class="breadcrumb-nav">
                         <li><a href="{{ route('home') }}"><i class="lni lni-home"></i> Home</a></li>
-                        <li>Login</li>
+                        <li>{{$product->name }}</li>
                     </ul>
                 </div>
             </div>
@@ -42,12 +42,10 @@
                     <div class="col-lg-6 col-md-12 col-12">
                         <div class="product-info">
                             <h2 class="title">{{$product->name}}</h2>
-                            <p class="category"><i class="lni lni-tag"></i> Drones:<a href="javascript:void(0)">Action
-                                    cameras</a></p>
-                            <h3 class="price">$850<span>$945</span></h3>
-                            <p class="info-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                tempor incididunt
-                                ut labore et dolore magna aliqua.</p>
+                            <p class="category"><i class="lni lni-tag"></i> {{ $product->category->name }}:<a href="javascript:void(0)">
+                                    {{ $product->category->name }}</a></p>
+                            <h3 class="price">{{$product->price}}<span>{{ $product->compare_price }}</span></h3>
+                            <p class="info-text">{{$product->description}}</p>
                             <div class="row">
                                 <div class="col-lg-4 col-md-4 col-12">
                                     <div class="form-group color-option">
